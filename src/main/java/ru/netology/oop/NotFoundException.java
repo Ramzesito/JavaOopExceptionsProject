9e7aa26d93bbab19957f6a6834a90ae8f743a92d
Ramzesito <roman_shv@mail.ru>
2023-06-13 22:51:47 +0300

    Init commit

---
+++ src/main/java/ru/netology/oop/NotFoundException.java
@@ -0,0 +1,9 @@
+package ru.netology.oop;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String msg) {
+        super(msg);
+    }
+
+}
---
+++ src/test/java/ru/netology/oop/ShopRepositoryTest.java
@@ -0,0 +1,41 @@
+package ru.netology.oop;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ShopRepositoryTest {
+
+    Product product1 = new Product(11, "Product 11", 1111);
+    Product product2 = new Product(2, "Product 2", 222);
+    Product product3 = new Product(333, "Product 333", 33);
+    Product product4 = new Product(4444, "Product 4444", 4);
+
+    @Test
+    public void shouldDeleteProductByValidId() {
+        ShopRepository repository = new ShopRepository();
+        repository.add(product1);
+        repository.add(product2);
+        repository.add(product3);
+        repository.add(product4);
+        repository.remove(333);
+        Product[] expected = {product1, product2, product4};
+
+        Assertions.assertArrayEquals(expected, repository.findAll());
+    }
+
+    @Test
+    public void shouldThrowExceptionIfInvalidId() {
+        ShopRepository repository = new ShopRepository();
+        repository.add(product1);
+        repository.add(product2);
+        repository.add(product3);
+        repository.add(product4);
+        Product[] expected = {product1, product2, product4};
+
+        Assertions.assertThrows(NotFoundException.class, () -> {
+            repository.remove(3333);
+        });
+    }
+
+
+}
